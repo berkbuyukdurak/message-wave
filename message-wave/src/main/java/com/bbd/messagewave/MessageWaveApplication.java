76fd6d482661afd8bf375e4f3c73e13e94af1436
berkbuyukdurak <berkbuyukdurak07@gmail.com>
2024-08-03 19:42:13 +0300

    project initial commit

---
+++ message-wave/src/main/java/com/bbd/messagewave/MessageWaveApplication.java
@@ -0,0 +1,13 @@
+package com.bbd.messagewave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageWaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageWaveApplication.class, args);
+    }
+
+}
---
+++ message-wave/src/test/java/com/bbd/messagewave/MessageWaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbd.messagewave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageWaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
