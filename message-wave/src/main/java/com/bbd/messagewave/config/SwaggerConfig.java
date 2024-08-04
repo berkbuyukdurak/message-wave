98cf3823b639bfb4bd643a2c063a3d8aef8dbbd9
berkbuyukdurak <berkbuyukdurak07@gmail.com>
2024-08-04 12:34:12 +0300

    swagger config is implemented.

---
+++ message-wave/src/main/java/com/bbd/messagewave/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.bbd.messagewave.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public-apis")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("MessageWave API Docs").version("v1"));
+    }
+}
