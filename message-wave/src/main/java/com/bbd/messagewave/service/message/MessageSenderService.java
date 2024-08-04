3a1367170cc65285da5166b93fff8eacaee0ce38
berkbuyukdurak <berkbuyukdurak07@gmail.com>
2024-08-04 20:44:17 +0300

    sender service is refactored.

--- message-wave/src/main/java/com/bbd/messagewave/service/message/MessageSenderService.java
+++ message-wave/src/main/java/com/bbd/messagewave/service/message/MessageSenderService.java
@@ -1,6 +1,8 @@
 package com.bbd.messagewave.service.message;
 
+import com.bbd.messagewave.model.dto.message.request.WebhookRequestDTO;
 import com.bbd.messagewave.model.entity.Message;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 public class MessageSenderService {
 
     private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
 
     @Value("${messagewave.message.sender.webhook.url}")
     private String webhookUrl;
@@ -17,7 +20,8 @@ public class MessageSenderService {
     @Value("${messagewave.message.sender.auth.key}")
     private String webhookAuthKey;
 
-    public MessageSenderService() {
+    public MessageSenderService(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
         this.restTemplate = new RestTemplate();
     }
 
@@ -26,17 +30,20 @@ public class MessageSenderService {
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.set("x-ins-auth-key", webhookAuthKey);
 
-        String payload = String.format("{\"to\":\"%s\", \"content\":\"%s\"}",
-                message.getRecipientPhoneNumber(), message.getContent());
-
-        HttpEntity<String> request = new HttpEntity<>(payload, headers);
+        WebhookRequestDTO webhookRequest = new WebhookRequestDTO();
+        webhookRequest.setTo(message.getRecipientPhoneNumber());
+        webhookRequest.setContent(message.getContent());
 
         try {
+            String payload = objectMapper.writeValueAsString(webhookRequest);
+            HttpEntity<String> request = new HttpEntity<>(payload, headers);
+
             ResponseEntity<String> response = restTemplate.postForEntity(webhookUrl, request, String.class);
             if (response.getStatusCode() == HttpStatus.ACCEPTED && response.getBody() != null) {
                 return true;
             }
         } catch (Exception e) {
+            System.out.println(e.getMessage());
             return false;
         }
         return false;
