9ad0853ba5d38c4c0fc914c7166a84415457038a
berkbuyukdurak <berkbuyukdurak07@gmail.com>
2024-08-04 15:53:28 +0300

    Get sent messages endpoint is added to the message controller.

--- message-wave/src/main/java/com/bbd/messagewave/controller/MessageController.java
+++ message-wave/src/main/java/com/bbd/messagewave/controller/MessageController.java
@@ -2,6 +2,7 @@ package com.bbd.messagewave.controller;
 
 import com.bbd.messagewave.model.GenericApiResponse;
 import com.bbd.messagewave.model.dto.message.response.GetAllMessagesResponseDTO;
+import com.bbd.messagewave.model.dto.message.response.GetAllSentMessagesResponseDTO;
 import com.bbd.messagewave.model.entity.Message;
 import com.bbd.messagewave.service.MessageService;
 import com.bbd.messagewave.util.GenericResponseHandler;
@@ -30,6 +31,7 @@ public class MessageController {
         this.messageService = messageService;
     }
 
+    @GetMapping
     @Operation(summary = "Get All Messages", description = "Returns all the messages stored in the system.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successfully retrieved list of messages",
@@ -42,7 +44,6 @@ public class MessageController {
                     content = @Content(mediaType = "application/json",
                             schema = @Schema(implementation = GenericApiResponse.class)))
     })
-    @GetMapping
     public ResponseEntity<GenericApiResponse<List<GetAllMessagesResponseDTO>>> getAllMessages() {
         try {
             List<GetAllMessagesResponseDTO> messages = messageService.getAllMessages();
@@ -50,6 +51,31 @@ public class MessageController {
                 return GenericResponseHandler.errorResponse(HttpStatus.NOT_FOUND, "Messages not found");
             }
             return GenericResponseHandler.successResponse(HttpStatus.OK,"Successfully retrieved messages", messages);
+        } catch (Exception e) {
+            return GenericResponseHandler.errorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/sent")
+    @Operation(summary = "Retrieve Sent Messages", description = "Retrieves a list of messages that have been sent and cached.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved sent messages",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = GenericApiResponse.class))),
+            @ApiResponse(responseCode = "404", description = "No sent messages found",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = GenericApiResponse.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = GenericApiResponse.class)))
+    })
+    public ResponseEntity<GenericApiResponse<List<GetAllSentMessagesResponseDTO>>> getAllSentMessages() {
+        try {
+            List<GetAllSentMessagesResponseDTO> sentMessages = messageService.getAllSentMessages();
+            if (sentMessages.isEmpty()) {
+                return GenericResponseHandler.errorResponse(HttpStatus.NOT_FOUND, "No sent messages found");
+            }
+            return GenericResponseHandler.successResponse(HttpStatus.OK, "Successfully retrieved sent messages", sentMessages);
         } catch (Exception e) {
             return GenericResponseHandler.errorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error: " + e.getMessage());
         }
