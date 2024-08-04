d5a63c806072735747b7d2359973bb105c380d4e
berkbuyukdurak <berkbuyukdurak07@gmail.com>
2024-08-04 15:53:07 +0300

    message service is refactored and get all sent messages service function is added.

--- message-wave/src/main/java/com/bbd/messagewave/service/MessageService.java
+++ message-wave/src/main/java/com/bbd/messagewave/service/MessageService.java
@@ -1,23 +1,32 @@
 package com.bbd.messagewave.service;
 
-import com.bbd.messagewave.constants.MessageSendingStatus;
 import com.bbd.messagewave.model.dto.message.response.GetAllMessagesResponseDTO;
+import com.bbd.messagewave.model.dto.message.response.GetAllSentMessagesResponseDTO;
 import com.bbd.messagewave.model.entity.Message;
 import com.bbd.messagewave.repository.MessageRepository;
-import org.springframework.scheduling.annotation.Scheduled;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
 public class MessageService {
 
     private final MessageRepository messageRepository;
+    private final RedisService redisService;
+    private final ObjectMapper objectMapper;
 
-    public MessageService(MessageRepository messageRepository) {
+    @Value("${messagewave.redis.key.sentMessagePrefix}")
+    private String sentMessagePrefix;
+
+    public MessageService(MessageRepository messageRepository, RedisService redisService, ObjectMapper objectMapper) {
         this.messageRepository = messageRepository;
+        this.redisService = redisService;
+        this.objectMapper = objectMapper;
     }
 
     public List<GetAllMessagesResponseDTO> getAllMessages() {
@@ -25,6 +34,21 @@ public class MessageService {
         return messages.stream()
                 .map(this::convertToGetAllMessagesResponseDTO)
                 .collect(Collectors.toList());
+    }
+
+    public List<GetAllSentMessagesResponseDTO> getAllSentMessages() {
+        Set<String> keys = redisService.getKeys(sentMessagePrefix + "*");
+        List<GetAllSentMessagesResponseDTO> sentMessages = new ArrayList<>();
+        for (String key : keys) {
+            String jsonData = redisService.getValue(key);
+            try {
+                GetAllSentMessagesResponseDTO message = objectMapper.readValue(jsonData, GetAllSentMessagesResponseDTO.class);
+                sentMessages.add(message);
+            } catch (Exception e) {
+                // Handle JSON parsing errors
+            }
+        }
+        return sentMessages;
     }
 
     private GetAllMessagesResponseDTO convertToGetAllMessagesResponseDTO(Message message) {
