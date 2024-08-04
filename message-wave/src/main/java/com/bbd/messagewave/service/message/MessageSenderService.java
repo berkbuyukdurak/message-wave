5dbaf6e0609c05c44149a3251a401fbee2591386
berkbuyukdurak <berkbuyukdurak07@gmail.com>
2024-08-04 16:12:36 +0300

    message sender service is updated.

--- message-wave/src/main/java/com/bbd/messagewave/service/message/MessageSenderService.java
+++ message-wave/src/main/java/com/bbd/messagewave/service/message/MessageSenderService.java
@@ -2,8 +2,7 @@ package com.bbd.messagewave.service.message;
 
 import com.bbd.messagewave.model.entity.Message;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,17 +14,31 @@ public class MessageSenderService {
     @Value("${messagewave.message.sender.webhook.url}")
     private String webhookUrl;
 
+    @Value("${messagewave.message.sender.auth.key}")
+    private String webhookAuthKey;
+
     public MessageSenderService() {
         this.restTemplate = new RestTemplate();
     }
 
     public boolean sendMessage(Message message) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("x-ins-auth-key", webhookAuthKey);
+
+        String payload = String.format("{\"to\":\"%s\", \"content\":\"%s\"}",
+                message.getRecipientPhoneNumber(), message.getContent());
+
+        HttpEntity<String> request = new HttpEntity<>(payload, headers);
+
         try {
-            // Simulate sending the message
-            ResponseEntity<String> response = restTemplate.postForEntity(webhookUrl, message, String.class);
-            return response.getStatusCode() == HttpStatus.ACCEPTED;
+            ResponseEntity<String> response = restTemplate.postForEntity(webhookUrl, request, String.class);
+            if (response.getStatusCode() == HttpStatus.ACCEPTED && response.getBody() != null) {
+                return true;
+            }
         } catch (Exception e) {
             return false;
         }
+        return false;
     }
 }
