7ba4ab52b39d63402bd07c65dcf0389d67cbbb72
berkbuyukdurak <berkbuyukdurak07@gmail.com>
2024-08-04 12:58:04 +0300

    redis config is implemented.

---
+++ message-wave/src/main/java/com/bbd/messagewave/config/RedisConfig.java
@@ -0,0 +1,20 @@
+package com.bbd.messagewave.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.Jedis;
+
+@Configuration
+public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public Jedis jedis() {
+        return new Jedis(redisHost, redisPort);
+    }
+}
