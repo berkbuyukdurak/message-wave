5146471c2295edff00e602c628a52f0963cd2b49
berkbuyukdurak <berkbuyukdurak07@gmail.com>
2024-08-04 15:08:54 +0300

    redis service is implemented.

---
+++ message-wave/src/main/java/com/bbd/messagewave/service/RedisService.java
@@ -0,0 +1,31 @@
+package com.bbd.messagewave.service;
+
+import org.springframework.stereotype.Service;
+import redis.clients.jedis.Jedis;
+
+@Service
+public class RedisService {
+    private final Jedis jedis;
+
+    public RedisService(Jedis jedis) {
+        this.jedis = jedis;
+    }
+
+    /**
+     * Sets a value in Redis.
+     * @param key Redis key
+     * @param value Redis value
+     */
+    public void setValue(String key, String value) {
+        jedis.set(key, value);
+    }
+
+    /**
+     * Retrieves a value from Redis.
+     * @param key Redis key
+     * @return value or null if key does not exist
+     */
+    public String getValue(String key) {
+        return jedis.get(key);
+    }
+}
