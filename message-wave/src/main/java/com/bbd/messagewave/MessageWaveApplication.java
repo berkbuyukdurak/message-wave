42857040cb039a852a3447f9167ed8b122ff5f61
berkbuyukdurak <berkbuyukdurak07@gmail.com>
2024-08-04 15:38:21 +0300

    application startup logic is updated.

--- message-wave/src/main/java/com/bbd/messagewave/MessageWaveApplication.java
+++ message-wave/src/main/java/com/bbd/messagewave/MessageWaveApplication.java
@@ -1,13 +1,28 @@
 package com.bbd.messagewave;
 
+import com.bbd.messagewave.service.automation.AutomationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class MessageWaveApplication {
+@EnableScheduling
+public class MessageWaveApplication implements ApplicationListener<ContextRefreshedEvent> {
+
+    private final AutomationService automationService;
+
+    public MessageWaveApplication(AutomationService automationService) {
+        this.automationService = automationService;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MessageWaveApplication.class, args);
     }
 
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        automationService.startProcessing();
+    }
 }
